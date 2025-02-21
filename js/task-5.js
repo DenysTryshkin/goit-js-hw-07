110930ab938c6fc6fa5977d2549763d913eecdf9
Denys Tryshkin <denystryshkin@gmail.com>
2025-02-20 22:35:53 -0500

    added task-5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changeColor = document.querySelector(".change-color");
+const textColor = document.querySelector(".color");
+
+changeColor.addEventListener("click", () => {
+  const getRandomColor = getRandomHexColor();
+
+  document.body.style.background = getRandomColor;
+  textColor.textContent = getRandomColor;
+});
